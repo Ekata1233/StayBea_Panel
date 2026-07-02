6959413bec630b69438d5c62a1d912a2bab13703
Ekata1233 <jadhavjanki8462@gmail.com>
2026-07-02 13:30:02 +0530

    vercel error

--- src/utils/imagekit.ts
+++ src/utils/imagekit.ts
@@ -1,11 +1,9 @@
-// This file should only be used on the server (API routes, server components)
 import ImageKit from "imagekit";
 
-// For server-side usage only
 const imagekit = new ImageKit({
-  publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY!,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
-  urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT!,
 });
 
 export default imagekit;
